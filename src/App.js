1b96650ff989e83cbb3ea401ed2ad010183c30aa
John <john1625b@gmail.com>
2022-03-04 17:09:54 -0500

    adds wines

--- src/App.js
+++ src/App.js
@@ -1,66 +1,27 @@
 import './App.css';
 import {useEffect, useState, useCallback, memo} from "react";
 
-const Wine = memo(({winery, wine, average, reviews, image}) => {
-    console.log('rendered')
-    const numStars = parseInt(average[0])
+const User = ({email, first_name, last_name, avatar}) => {
     return (
-        <div className='wine'>
-            <img src={image} alt='wine' height='100px' />
-            <div>wine name: {wine && wine}</div>
-            <div>winery: {winery && winery}</div>
-            <div>
-                <p>reviews:</p>
-                <div>{numStars !== null && '★'.repeat(numStars) }</div>
-                <div>{reviews && reviews}</div>
-            </div>
-        </div>
+        <div>{email}</div>
     )
-})
+}
 
 function App() {
-    const [wines, setWines] = useState(null)
-    const [wineType, setWineType] = useState('reds')
-    const [nameSearch, setNameSearch] = useState('')
-    const fetchData = async (wineType) => {
-        let res = await fetch(`https://api.sampleapis.com/wines/${wineType}`)
+    const [users, setUsers] = useState([])
+    const fetchData = async () => {
+        let res = await fetchData('https://reqres.in/api/users?page=2')
         res = await res.json()
-        setWines(res.slice(0,10))
+        setUsers(res.data)
     }
-    useEffect(() => {
-        fetchData(wineType)
-    }, [])
-    useEffect(() => {
-        fetchData(wineType)
-    },[wineType])
-    const filterWines = (data, nameSearch) => {
-        return data.filter(wine => {
-            return wine?.wine?.toLowerCase().includes(nameSearch)
-        })
-    }
-    // console.log(wines && wines[0])
-    const wineTypesList = ['reds', 'whites', 'sparkling', 'rose', 'dessert', 'port']
     return (
         <div className='App'>
-            <h1>wines with choices</h1>
-            <form>
-                <label>
-                    Search by Name:
-                    <input type="text" value={nameSearch} onChange={e => setNameSearch(e.target.value.toLowerCase())} />
-                </label>
-            </form>
-            <h2>selected wine: {wineType}</h2>
+            <h1>Users</h1>
             {
-                wineTypesList.map(type => (
-                    <button key={type} onClick={() => setWineType(type)}>{type}</button>
-                ))
-            }
-            {
-                wines ? filterWines(wines, nameSearch).map(({winery, wine, rating, location, image, id}) => (
-                    <Wine key={id} winery={winery} wine={wine} average={rating.average} reviews={rating.reviews}
-                          location={location} image={image}/>
-                ))
-                : <div>Loading...</div>
+                users ? users.map(({id, email, first_name, last_name, avatar}) => (
+                    <User key={id} email={email}/>
+                    ))
+                    : <div>Loading...</div>
             }
         </div>
     )
